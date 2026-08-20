c85190f0496807a31983a5b52c10e31ef892dfe6
chuan_liu_ya <chuan_liu_ya@163.com>
2026-08-20 14:08:04 +0800

    🌈 style: 初步添加darktheme

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import { zhCN, dateZhCN } from 'naive-ui'
+import { zhCN, dateZhCN, darkTheme } from 'naive-ui'
 import AppFrame from './views/layout/AppFrame.vue'
-
 </script>
 
 <template>
-  <!-- 全局 Provider：配置主题/语言，并让 Message/Dialog/Notification/LoadingBar 等 API 组件全局可用 -->
-  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN" :theme="darkTheme">
     <n-loading-bar-provider>
       <n-message-provider>
         <n-dialog-provider>
           <n-notification-provider>
-            <!-- 应用布局壳：左侧导航 + 页面路由 -->
             <AppFrame />
           </n-notification-provider>
         </n-dialog-provider>
